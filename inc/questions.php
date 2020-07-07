441c5ad0aff2936097d2a09f4dc5145ba320c6b7
ChristopherSab <chris.sabaoth@protonmail.com>
2020-07-07 10:21:39 +0100

    Update

--- inc/questions.php
+++ inc/questions.php
@@ -1,11 +1,20 @@
 <?php
 
-$questions = [];
 
-for($i=0; $i < 10; $i++){
-    $questions[$i]['leftAdder'] = rand(10, 100);
-    $questions[$i]['rightAdder'] = rand(10, 100);
-    $questions[$i]['correctAnswer'] = $questions[$i]['leftAdder'] + $questions[$i]['rightAdder'];
-    $questions[$i]['firstIncorrectAnswer'] = $questions[$i]['correctAnswer'] + 11;
-    $questions[$i]['secondIncorrectAnswer'] = $questions[$i]['correctAnswer'] - 9;
+
+function getRandomQuestion(){
+
+    $questions = array();
+
+    for($i=0; $i < 10; $i++){
+        $questions[$i]['leftAdder'] = rand(10, 100);
+        $questions[$i]['rightAdder'] = rand(10, 100);
+        $questions[$i]['correctAnswer'] = $questions[$i]['leftAdder'] + $questions[$i]['rightAdder'];
+        $questions[$i]['firstIncorrectAnswer'] = $questions[$i]['correctAnswer'] + 11;
+        $questions[$i]['secondIncorrectAnswer'] = $questions[$i]['correctAnswer'] - 9;
+    }
+    
+    return $questions;
 }
+
+
--- inc/quiz.php
+++ inc/quiz.php
@@ -5,21 +5,23 @@ session_start();
 // Question from "questions.php" file
 include('inc/questions.php');
 
+//An array of 10 random questions is saved to the Session
+$_SESSION["questions"] = getRandomQuestion();
+
+
 // Determines if the score will be shown or not. Set to false by default
 $show_score = False;
 
 // Total number of questions to ask
-$totalQuestions = count($questions);
+$totalQuestions = count($_SESSION["questions"]);
 
 // Toast message which is displayed to the page after a user chooses answer, set to empty by default
 $toast = null;
 
-// Variable to holds a random index index number.
-
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-  if ($_POST['answer'] == $questions[$_POST['id']]["correctAnswer"]) {
+  if ($_POST['answer'] == $_SESSION["questions"][$_POST['id']]["correctAnswer"]){
     $toast = "<h2 class='correct'> Correct Answer, Well Done! &#9786 </h2>";
     $_SESSION['totalCorrect'] += 1;
   } else {
@@ -34,10 +36,10 @@ if(!isset($_SESSION['used_indexes'])){
   $_SESSION['totalCorrect'] = 0;
 }
 
+
 if(count($_SESSION['used_indexes']) == $totalQuestions){
   $_SESSION['used_indexes'] = [];
   $show_score = true;
-
 } else {
   $show_score = false;
     if(count($_SESSION['used_indexes']) == 0){
@@ -47,7 +49,8 @@ if(count($_SESSION['used_indexes']) == $totalQuestions){
 
   $index = rand(0, $totalQuestions - 1);
   
-  $question = $questions[$index];
+  $question = $_SESSION["questions"][$index];
+
   array_push($_SESSION['used_indexes'], $index);
 
   $answers = [
@@ -60,4 +63,5 @@ shuffle($answers);
 
 }
 
- // session_destroy();
+//  session_destroy();
+
