f6d786b3f2f7bb5f95eb2008dce60be413b9cbb4
ChristopherSab <chris.sabaoth@protonmail.com>
2020-07-07 16:23:54 +0100

    Completed Project 2 Ready for Submission

--- inc/questions.php
+++ inc/questions.php
@@ -10,10 +10,23 @@ function getRandomQuestion(){
         $questions[$i]['leftAdder'] = rand(10, 100);
         $questions[$i]['rightAdder'] = rand(10, 100);
         $questions[$i]['correctAnswer'] = $questions[$i]['leftAdder'] + $questions[$i]['rightAdder'];
-        $questions[$i]['firstIncorrectAnswer'] = $questions[$i]['correctAnswer'] + 11;
-        $questions[$i]['secondIncorrectAnswer'] = $questions[$i]['correctAnswer'] - 9;
+        $questions[$i]['firstIncorrectAnswer'] = null;
+        $questions[$i]['secondIncorrectAnswer'] = $questions[$i]['correctAnswer'] - rand(3, 11);
+
+        if($questions[$i]['correctAnswer'] !== $questions[$i]['firstIncorrectAnswer'] ){
+            $questions[$i]['firstIncorrectAnswer'] = rand(10, 200);
+        }else {
+            $questions[$i]['firstIncorrectAnswer'] = $questions[$i]['correctAnswer'] + rand(3, 19);
+        }
+
+        if($questions[$i]['correctAnswer'] !== $questions[$i]['secondIncorrectAnswer'] ){
+            $questions[$i]['secondIncorrectAnswer'] = rand(10, 200);
+        }else {
+            $questions[$i]['secondIncorrectAnswer'] = $questions[$i]['correctAnswer'] + rand(3, 19);
+        }
     }
-    
+
+
     return $questions;
 }
 
--- inc/quiz.php
+++ inc/quiz.php
@@ -5,10 +5,6 @@ session_start();
 // Question from "questions.php" file
 include('inc/questions.php');
 
-//An array of 10 random questions is saved to the Session
-//$_SESSION["questions"] = getRandomQuestion();
-
-
 // Determines if the score will be shown or not. Set to false by default
 $show_score = False;
 
@@ -20,6 +16,7 @@ $toast = null;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+  //Checks to see if the answer selected is the correct answer in the questions array & displays relevant message
  if ($_POST['answer'] == $_SESSION["questions"][$_POST['id']]["correctAnswer"]){
     $toast = "<h2 class='correct'> Correct Answer, Well Done! &#9786 </h2>";
     $_SESSION['totalCorrect'] += 1;
@@ -46,8 +43,10 @@ if(count($_SESSION['used_indexes']) == $totalQuestions){
       $toast = '';
     }
 
-  $index = rand(0, $totalQuestions - 1);
-  
+    do{
+      $index = rand(0, $totalQuestions - 1);
+    }while(in_array($index, $_SESSION['used_indexes']));
+
   $question = $_SESSION["questions"][$index];
 
   array_push($_SESSION['used_indexes'], $index);
@@ -63,4 +62,6 @@ shuffle($answers);
 }
 
 //  session_destroy();
+
+
 
